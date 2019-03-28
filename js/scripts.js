83263df43b88b218ed2a043c49219ca5c8aa3fe1
Joe Barnes and Liz Kelley <joe@>
2019-03-28 13:04:58 -0700

    fix show bugs

--- js/scripts.js
+++ js/scripts.js
@@ -3,59 +3,37 @@ $(function(){
     var ageInput = parseInt($('input#age').val());
     var adventureType = $('#adventure-type').val();
 
+    $('.TOS').hide();
+    $('.TNG').hide();
+    $('.DS9').hide();
+
     if(ageInput >= 50 && adventureType === "Cheesy"){
       $('.TOS').show('slow');
-      // $('.TNG').remove('.TNG');
-      // $('.DS9').remove('.DS9');
-
 
     }else if (ageInput > 50 && adventureType === "Adventurous"){
       $('.TNG').show('slow');
-      // $('.TOS').remove('.TOS');
-      // $('.DS9').remove('.DS9');
 
     }else if (ageInput > 50 && adventureType === "Battle!"){
       $('.DS9').show('slow');
-      // $('.TNG').remove('.TNG');
-      // $('.TOS').remove('.TOS');
 
     }else if (ageInput >= 35 && ageInput <=49 && adventureType ==="Cheesy"){
       $('.TOS').show('slow');
-      // $('.TNG').remove('.TNG');
-      // $('.TOS').remove('.TOS');
 
     }else if (ageInput >=35 && ageInput <=49 && adventureType === "Adventurous"){
       $('.TNG').show('slow');
-      // $('.TNG').remove('.TNG');
-      // $('.TOS').remove('.TOS');
 
     }else if (ageInput >=35 && ageInput <=49 && adventureType === "Battle!"){
       $('.DS9').show('slow');
-      // $('.TNG').remove('.TNG');
-      // $('.TOS').remove('.TOS');
 
     }else if (ageInput < 35 && adventureType === "Cheesy"){
       $('.TOS').show('slow');
-      // $('.TNG').remove('.TNG');
-      // $('.TOS').remove('.TOS');
 
     }else if (ageInput < 35  && adventureType === "Adventurous"){
       $('.TNG').show('slow');
-      // $('.TNG').remove('.TNG');
-      // $('.TOS').remove('.TOS');
 
     }else if (ageInput < 35  && adventureType === "Battle!"){
       $('.DS9').show('slow');
-      // $('.TNG').remove('.TNG');
-      // $('.TOS').remove('.TOS');
     }
-
-  // $('.btn').click(function(){
-  //   $('.TNG').remove();
-  //   $('.DS9').remove();
-  //   $('.TOS').remove();
-  // })
-
     event.preventDefault();
   });
 });
