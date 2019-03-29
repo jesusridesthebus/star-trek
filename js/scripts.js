16da7208e182819f6bd46c8447050d45be89f655
Joe Barnes and Liz Kelley <liz@>
2019-03-28 18:10:02 -0700

    add logic to js to hide the other captains when you change quiz answers. add variables in order to make the captain quiz result logic, which does not work yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,69 @@
 $(function(){
   $("#formOne").submit(function(event){
-    var ageInput = parseInt($('input#age').val());
-    var adventureType = $('#adventure-type').val();
+    $(".TOS").hide();
+    $(".TNG").hide();
+    $(".DS9").hide();
 
-    $('.TOS').hide();
-    $('.TNG').hide();
-    $('.DS9').hide();
+    var ageInput = parseInt($("input#age").val());
+    var adventureType = $("#adventure-type").val();
 
     if(ageInput >= 50 && adventureType === "Cheesy"){
-      $('.TOS').show('slow');
+      $(".TOS").show("slow");
 
-    }else if (ageInput > 50 && adventureType === "Adventurous"){
-      $('.TNG').show('slow');
+    } else if (ageInput > 50 && adventureType === "Adventurous"){
+      $(".TNG").show("slow");
 
-    }else if (ageInput > 50 && adventureType === "Battle!"){
-      $('.DS9').show('slow');
+    } else if (ageInput > 50 && adventureType === "Battle!"){
+      $(".DS9").show("slow");
 
-    }else if (ageInput >= 35 && ageInput <=49 && adventureType ==="Cheesy"){
-      $('.TOS').show('slow');
+    } else if (ageInput >= 35 && ageInput <=49 && adventureType ==="Cheesy"){
+      $(".TOS").show("slow");
 
-    }else if (ageInput >=35 && ageInput <=49 && adventureType === "Adventurous"){
-      $('.TNG').show('slow');
+    } else if (ageInput >=35 && ageInput <=49 && adventureType === "Adventurous"){
+      $(".TNG").show("slow");
 
-    }else if (ageInput >=35 && ageInput <=49 && adventureType === "Battle!"){
-      $('.DS9').show('slow');
+    } else if (ageInput >=35 && ageInput <=49 && adventureType === "Battle!"){
+      $(".DS9").show("slow");
 
-    }else if (ageInput < 35 && adventureType === "Cheesy"){
-      $('.TOS').show('slow');
+    } else if (ageInput < 35 && adventureType === "Cheesy"){
+      $(".TOS").show("slow");
 
-    }else if (ageInput < 35  && adventureType === "Adventurous"){
-      $('.TNG').show('slow');
+    } else if (ageInput < 35  && adventureType === "Adventurous"){
+      $(".TNG").show("slow");
 
-    }else if (ageInput < 35  && adventureType === "Battle!"){
-      $('.DS9').show('slow');
+    } else if (ageInput < 35  && adventureType === "Battle!"){
+      $(".DS9").show("slow");
     }
     event.preventDefault();
   });
 
   $("#captain-quiz").submit(function(event) {
+    event.preventDefault();
+    $(".captain-picard").hide();
+    $(".captain-kirk").hide();
+    $(".captain-janeway").hide();
+
     var diplomacy = $("input:radio[name=first-question]:checked").val();
     var firstOfficer = $("input:radio[name=second-question]:checked").val();
     var klingon = $("input:radio[name=third-question]:checked").val();
 
-    $(".picard").hide();
-    $(".kirk").hide();
-    $(".janeway").hide();
+    var diplomacyKirk = $("input:radio[name=first-question]:checked").attr("kirk1");
+    var firstOfficerKirk = $("input:radio[name=first-question]:checked").attr("kirk2");
+    var klingonKirk = $("input:radio[name=first-question]:checked").attr("kirk3");
 
-    if(diplomacy === "kirk")
-    console.log(kirk);
+    var diplomacyPicard = $("input:radio[name=second-question]:checked").attr("picard1");
+    var firstOfficerPicard = $("input:radio[name=second-question]:checked").attr("picard2");
+    var klingonPicard = $("input:radio[name=second-question]:checked").attr("picard3");
+
+    var diplomacyJaneway = $("input:radio[name=third-question]:checked").attr("janeway1");
+    var firstOfficerJaneway = $("input:radio[name=third-question]:checked").attr("janeway2");
+    var klingonJaneway = $("input:radio[name=third-question]:checked").attr("janeway3");
+
+    if(diplomacy === diplomacyKirk && firstOfficer === diplomacyKirk && klingon === klingonKirk) {
+      $(".captain-kirk").show();
+    }
+    console.log("diplomacy", diplomacy);
+    console.log("firstOfficer", firstOfficer);
+    console.log("klingon", klingon);
   });
 });
